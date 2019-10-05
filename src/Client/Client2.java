931200569645bda4416f8eb6a253d7ad35a39700
lukeyuan31 <1393499408@qq.com>
2019-10-05 12:41:28 -0400

    finished upload and get(will cause socket to close)

---
+++ src/Client/Client2.java
@@ -0,0 +1,231 @@
+package Client;
+
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class Client2 {
+    Socket requestSocket;           //socket connect to the server
+    ObjectOutputStream out;         //stream write to the socket
+    ObjectInputStream in;          //stream read from the socket
+    String message;                //message send to the server
+    String MESSAGE;                //capitalized message read from the server
+    boolean loginStatus=false;
+
+    public void Client() {}
+
+    void run()
+    {
+        try{
+            //create a socket to connect to the server
+            //requestSocket = new Socket("localhost", 8000);
+            //System.out.println("Connected to localhost in port 8000");
+            //initialize inputStream and outputStream
+           // out = new ObjectOutputStream(requestSocket.getOutputStream());
+            //out.flush();
+            //in = new ObjectInputStream(requestSocket.getInputStream());
+            /*
+            FileInputStream fis=new FileInputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Client/test.txt");
+            byte[] bytes = new byte[1024];
+            int data;
+            while((data = fis.read(bytes))!= -1){
+                out.write(bytes, 0, data);
+            }
+            fis.close();
+            */
+
+            //get Input from standard input
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            while(true)
+            {
+                if(requestSocket==null){
+                    requestSocket = new Socket("localhost",8000);
+                    out = new ObjectOutputStream(requestSocket.getOutputStream());
+                    out.flush();
+                    in = new ObjectInputStream(requestSocket.getInputStream());
+                }
+                else {
+                    //System.out.print("Hello, please input a sentence: ");
+                    System.out.println("Please select your move \n1. login\n2. dir\n3. get\n4. upload");
+                    //read a sentence from the standard input
+                    message = bufferedReader.readLine();
+                    //Send the sentence to the server
+                    //sendMessage(message);
+                    switch (message) {
+                        case ("1"): {
+                            if (loginStatus == false) {
+                                sendMessage("login");
+                                String response = (String) in.readObject();
+                                if (response.equals("success")) {
+                                    System.out.println("Login successfully!");
+                                    loginStatus = true;
+                                }
+                                break;
+                            } else {
+                                System.out.println("You are already logged in");
+                                break;
+                            }
+                        }
+                        case ("2"): {
+                            if (loginStatus == true) {
+                                sendMessage("dir");
+                                String response = (String) in.readObject();
+                                System.out.println(response);
+                                break;
+                            } else {
+                                System.out.println("You need to login first");
+                                break;
+                            }
+                        }
+                        case ("3"): {
+                            if (loginStatus == false) {
+                                System.out.println("You need to login first");
+                                break;
+                            } else {
+                                sendMessage("get");
+                                System.out.println("Input the name of the file you want to get");
+                                String filename = bufferedReader.readLine();
+                                sendMessage(filename);
+                                FileOutputStream fos = new FileOutputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Client/test1.txt");
+                                InputStream is = requestSocket.getInputStream();
+                                //System.out.println("file aquired!");
+                                byte[] bytes = new byte[1024];
+                                int data;
+                                while ((data = is.read()) != -1) {
+                                    //System.out.println(data);
+                                    fos.write(data);
+                                }
+                                //fos.write(-1);
+                                fos.close();
+                                requestSocket.close();
+                                requestSocket=null;
+                                break;
+                            }
+                        }
+                        case ("4"): {
+                            if (loginStatus == false) {
+                                System.out.println("You need to login first");
+                                break;
+                            } else {
+                                sendMessage("upload");
+                                System.out.println("Input the name of the file you want to upload");
+                                String filename = bufferedReader.readLine();
+                                sendMessage(filename);
+                                File dir = new File(filename);
+                                String status = (String) in.readObject();
+                                if (status.equals("ready")) {
+                                    String directory = dir.getAbsolutePath();
+                                    OutputStream os = requestSocket.getOutputStream();
+                                    FileInputStream fis = new FileInputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Client/test.txt");
+                                    byte[] bytes = new byte[1024];
+                                    int data;
+                                    while ((data = fis.read(bytes)) != -1) {
+                                        os.write(bytes,0,data);
+                                        // System.out.println(data);
+                                    }
+                                    //os.write(-1);
+                                    os.flush();
+                                    requestSocket.close();
+                                    requestSocket=null;
+                                    //fis.close();
+                                    //os.flush();
+                                    //requestSocket.shutdownInput();
+
+                                    //requestSocket=null;
+                                    break;
+                                }
+                            }
+                        }
+
+
+                    }
+                    //Receive the upperCase sentence from the server
+                    //MESSAGE = (String)in.readObject();
+                    //show the message to the user
+                /*
+                if (MESSAGE.equals("1")){
+                    System.out.println("you need to login");
+                }else {
+                    String[] temp=message.split(" ");
+                    String command=temp[0];
+                    switch (command){
+                        case ("get"):{
+                            System.out.println("into get");
+                            break;
+                        }
+                        case ("upload"):{
+                            System.out.println("into upload");
+
+                            String filename=temp[1];
+                            File dir=new File(filename);
+                            String directory=dir.getAbsolutePath();
+                            //System.out.println(directory);
+                            FileInputStream fis = new FileInputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Client/test.txt");
+                            System.out.println(directory);
+                            byte[] bytes = new byte[1024];
+                            int data;
+                            while((data = fis.read(bytes))!= -1){
+                                out.write(bytes, 0, data);
+                            }
+                            fis.close();
+                            System.out.println("Upload complete!");
+                            //client.close();
+
+                            break;
+                        }
+                    }
+
+                    System.out.println("Receive message: " + MESSAGE);
+                }
+
+                 */
+                }
+            }
+        }
+        catch (ConnectException e) {
+            System.err.println("Connection refused. You need to initiate a server first.");
+        }
+        catch ( ClassNotFoundException e ) {
+            System.err.println("Class not found");
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //Close connections
+            try{
+                in.close();
+                out.close();
+                requestSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    //send a message to the output stream
+    void sendMessage(String msg)
+    {
+        try{
+            //stream write the message
+            out.writeObject(msg);
+            out.flush();
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    //main method
+    public static void main(String args[])
+    {
+        Client2 client = new Client2();
+        client.run();
+    }
+
+}
---
+++ src/Server/Server2.java
@@ -0,0 +1,240 @@
+package Server;
+
+import java.awt.*;
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class Server2 {
+
+    private static final int sPort = 8000;   //The server will be listening on this port number
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running.");
+        ServerSocket listener = new ServerSocket(sPort);
+        int clientNum = 1;
+        try {
+            while(true) {
+                new Handler(listener.accept(),clientNum).start();
+                //System.out.println("Client "  + clientNum + " is connected!");
+                clientNum++;
+            }
+        } finally {
+            listener.close();
+        }
+
+    }
+
+    /**
+     * A handler thread class.  Handlers are spawned from the listening
+     * loop and are responsible for dealing with a single client's requests.
+     */
+    private static class Handler extends Thread {
+        private String message;    //message received from the client
+        private String MESSAGE="";    //uppercase message send to the client
+        private Socket connection;
+        private ObjectInputStream in;	//stream read from the socket
+        private ObjectOutputStream out;    //stream write to the socket
+        private int no;		//The index number of the client
+
+        public Handler(Socket connection, int no) {
+            this.connection = connection;
+            this.no = no;
+        }
+
+        public boolean Login(String Username, String Password){
+            if (Username.equals("user1")) {
+                if (Password.equals("1")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }else{
+                System.out.println("Incorrect User");
+                return false;
+            }
+        }
+
+        public String[] getFilenames(){
+            File file=new File(".");
+            String [] names=file.list();
+            for (String name : names) {
+                System.out.println(name);
+            }
+            return names;
+        }
+
+        public void run() {
+            String UserName;
+            boolean loginStatus=false;
+            String command;
+            try{
+                //initialize Input and Output streams
+                out = new ObjectOutputStream(connection.getOutputStream());
+                out.flush();
+                in = new ObjectInputStream(connection.getInputStream());
+                /*
+                FileOutputStream fos = new FileOutputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Server/test.txt");
+               // System.out.println(directory);
+                System.out.println("file aquired!");
+                byte[] bytes = new byte[1024];
+                int data;
+                while ((data = in.read(bytes)) != -1) {
+                    fos.write(bytes, 0, data);
+                }
+                fos.close();
+
+                 */
+                try{
+                    while(true)
+                    {
+                        //receive the message sent from the client
+                        message = (String)in.readObject();
+                        String[] temp;
+                        temp=message.split(" ");
+                        command=temp[0];
+                        //System.out.println(command);
+                        switch (message) {
+                            case ("login"):
+                                //System.out.println("recognize 1");
+                                //String message = "input username and password";
+                                sendMessage("success");
+                                //loginStatus=true;
+                                break;
+                            case ("dir"): {
+                                    //System.out.println("dir test");
+                                    //MESSAGE=MESSAGE+"\n"+MESSAGE;
+                                    String[] result= getFilenames();
+                                    for (String name:result){
+                                       System.out.println(name);
+                                       MESSAGE=MESSAGE+"\n"+name;
+                                   }
+                                    sendMessage(MESSAGE);
+                                    break;
+
+                            }
+                            case ("upload"):{
+                                String filename = (String)in.readObject();
+                                //System.out.println(filename);
+                                File dir=new File(filename);
+                                sendMessage("ready");
+                                String directory=dir.getAbsolutePath();
+                                //System.out.println(directory);
+                                FileOutputStream fos = new FileOutputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Server/test.txt");
+                                System.out.println(directory);
+                                InputStream is=connection.getInputStream();
+                                //System.out.println("file aquired!");
+                                byte[] bytes = new byte[1024];
+                                int data;
+                                while ((data = is.read(bytes)) != -1) {
+                                    //System.out.println(data);
+                                    fos.write(bytes,0,data);
+                                }
+                                //fos.close();
+                                System.out.println("Upload complete!");
+                                break;
+                            }
+                            case ("get"):{
+                                String filename = (String)in.readObject();
+                                FileInputStream fis=new FileInputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Server/test1.txt");
+                                byte[] bytes = new byte[1024];
+                                int data;
+                                OutputStream os = connection.getOutputStream();
+                                while((data = fis.read(bytes))!= -1){
+                                    os.write(bytes, 0, data);
+                                    // System.out.println(data);
+                                }
+                                connection.close();
+                                fis.close();
+                                break;
+                            }
+                            /*
+                            case ("get"): {
+                                if (!loginStatus) {
+                                    sendMessage("1");
+                                    break;
+                                } else {
+                                    File dir=new File(".");
+                                    String directory=dir.getCanonicalPath();
+                                    //System.out.println("get test");
+                                    String filename=temp[1];
+                                    String filepath=directory+"/"+filename;
+                                    System.out.println(filepath);
+                                    sendMessage("get test");
+                                    break;
+                                }
+                            }
+                            case ("upload"): {
+                                if (!loginStatus) {
+                                    sendMessage("1");
+                                    break;
+                                } else {
+                                    //System.out.println("upload test");
+                                    sendMessage("upload test");
+                                    //InputStream is = accept.getInputStream();
+                                    String filename="test.txt";
+                                    File dir=new File(filename);
+                                    String directory=dir.getAbsolutePath();
+                                    FileOutputStream fos = new FileOutputStream("/Users/lukeyuan/IdeaProjects/TCPServerClient/src/Server/test.txt");
+                                    System.out.println(directory);
+                                    System.out.println("file aquired!");
+                                    byte[] bytes = new byte[1024];
+                                    int data;
+                                    while ((data = in.read(bytes)) != -1) {
+                                        fos.write(bytes, 0, data);
+                                    }
+                                    fos.close();
+                                    System.out.println("Upload complete!");
+                                    sendMessage("upload test");
+                                    break;
+                                }
+                            }
+                            */
+
+                        }
+                        //show the message to the user
+                        System.out.println("Receive message: " + message + " from client " + no);
+                        //Capitalize all letters in the message
+                        //MESSAGE = message.toUpperCase();
+                        //send MESSAGE back to the client
+                        //sendMessage(MESSAGE);
+                    }
+                }
+                catch(ClassNotFoundException classnot){
+                    System.err.println("Data received in unknown format");
+                }
+            }
+            catch(IOException ioException){
+                //System.out.println("Disconnect with Client " + no);
+            }
+            finally{
+                //Close connections
+                try{
+                    in.close();
+                    out.close();
+                    connection.close();
+                }
+                catch(IOException ioException){
+                    //System.out.println("Disconnect with Client " + no);
+                }
+            }
+        }
+
+        //send a message to the output stream
+        public void sendMessage(String msg)
+        {
+            try{
+                out.writeObject(msg);
+                out.flush();
+                System.out.println("Send message: " + msg + " to Client " + no);
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+
+    }
+
+}
